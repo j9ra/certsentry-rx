7ee0e74e1a8165d3c96f63df3d90f3d27334dead
Janusz Grabowski <jan@wit.edu.pl>
2022-07-11 21:48:37 +0200

    Fix test case

--- certsentryrx-data/src/test/java/pl/grabojan/certsentryrx/data/CertsentryrxDataApplicationTests.java
+++ certsentryrx-data/src/test/java/pl/grabojan/certsentryrx/data/CertsentryrxDataApplicationTests.java
@@ -2,8 +2,10 @@ package pl.grabojan.certsentryrx.data;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = DataConfig.class)
 class CertsentryrxDataApplicationTests {
 
 	@Test
