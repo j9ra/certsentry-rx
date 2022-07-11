da81c0895bfb3f8a0c6436ff97a21081781a403b
Janusz Grabowski <jan@wit.edu.pl>
2022-07-11 21:56:49 +0200

    r2dbc h2 test initialization

---
+++ certsentryrx-data/src/test/java/pl/grabojan/certsentryrx/data/R2DBCConfiguration.java
@@ -0,0 +1,20 @@
+package pl.grabojan.certsentryrx.data;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+
+import io.r2dbc.h2.H2ConnectionConfiguration;
+import io.r2dbc.h2.H2ConnectionFactory;
+
+public class R2DBCConfiguration extends AbstractR2dbcConfiguration {
+
+	  @Bean
+	    public H2ConnectionFactory connectionFactory() {
+	        return new H2ConnectionFactory(
+	            H2ConnectionConfiguration.builder()
+	              .url("mem:testdb;DB_CLOSE_DELAY=-1;")
+	              .username("sa")
+	              .build()
+	        );
+	    }
+}
