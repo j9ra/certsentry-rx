7144ddd129063dc7f5af0885e8be3fc1d51a1558
Janusz Grabowski <jan@wit.edu.pl>
2022-07-11 22:04:07 +0200

    utils test case initialization

--- certsentryrx-util/src/test/java/pl/grabojan/certsentryrx/util/CertsentryrxUtilApplicationTests.java
+++ certsentryrx-util/src/test/java/pl/grabojan/certsentryrx/util/CertsentryrxUtilApplicationTests.java
@@ -2,8 +2,10 @@ package pl.grabojan.certsentryrx.util;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = { CertServicesConfig.class, HttpClientConfig.class })
 class CertsentryrxUtilApplicationTests {
 
 	@Test
